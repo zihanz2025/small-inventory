cc72c28d99eb64ff69f095e1b8c768120476be3f
Zihan Zhu <zihanz1006@gmail.com>
2025-09-03 19:25:36 +0800

    initialize springboot

---
+++ small-inventory/src/test/java/com/zihan/small_inventory/SmallInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zihan.small_inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
